a31adbd47284417b39d5e422b211497815a01eb9
Lorena Tacal <lorena.tacal@gmail.com>
2018-05-28 14:57:11 +0100

    Finish functionality for the Pomodoro Clock - add Readme.md; - finish the html for the project; - finish JS for the project; - fix all edge cases;

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # Pomodoro Clock
 This is a project build using JavaScript
+
+####25.05.2018 
+1. ##### What I've learned today:
+	- to call a function inside the scope of a different function;
+	- break can be called only in loops not in if/else;
+	- from a function you can exit with return, but not from an each or from a for;
+	- to get the value of a DOM element using jquery;
+	- DRY principle;
+	- to pull projects from Github;
+	- to make a RETRO;
+2. ##### Tomorrow I want to do:
+	- make the startClock() to start after the timer of startPomodoro() reaches 0;
+	- refector and tidy my project;
+	- download and install Visual code;
+3. ##### I am blocked:
+
+#### 26.05.2018
+1. ##### What I've learned today:
+	- download and Install Visual Code;
+	- to auto indent my code in Visual Code;
+	- draw diagrams and undersand what is happening in the code;
+	- the difference between synchronous and asynchronous functions;
+	- the difference between event listeners and event handlers;
+	- the concept of callback and how to use it;
+	- to refector and tidy my code;
+2. ##### Tomorrow I want to do:
+	- fix the Edge Cases;
+	- learn the Git Flow;
+	- create a plan for the CSS;
+	- design the CSS;
+3. ##### I am blocked:
+
+#### 27.05.2018
+1. ##### What I've learned today:
+	- to fix the edge cases of my project;
+	- open my projects with Visual Code using PowerShell;
+2. ##### Tomorrow I want to:
+	- learn the Git Flow;
+	- create a plan for the CSS;
+	- implemet the CSS;
+3. ##### I am blocked: 
+
+
--- index.js
+++ index.js
@@ -1,67 +1,103 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var paused;
     var countdown;
     var sessionLength;
     var countdownTime;
-    var finalSessionLength
+    var finalSessionLength = 25;
+    var breakLength;
+    var finalBreakLength = 5;
+    var clockTimer;
 
-function startTimer(duration, display) {
-    var timer = duration
-    var minutes;
-    var seconds;
+    function createClock(duration, display, callback){
+        let minutes;
+        let seconds;
+        countdown = setInterval(function () {
+            minutes = parseInt(duration / 60, 10)
+            seconds = parseInt(duration % 60, 10);
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            display.text(minutes + ":" + seconds);
+
+            if (--duration <= 0) {
+                clearInterval(countdown);
+                callback();
+            }
+        }, 1000);
+    }
+
+    function startBreak() {
+        createClock($('#session-break').html() * 60, $('#countdown-timer'), startSessionLength);
+    }
+
+    function startSessionLength() {
+        createClock($('#session-length').html() * 60, $('#countdown-timer'), startBreak);
+    }
+
+    function startClock() {
+        $('.start-timer').on('click', function () {
+            paused = false;
+            $('body').append("<div id='countdown-timer'></div>");
+            countdownTime = 60 * finalSessionLength;
+            clearInterval(countdown);
+            createClock(countdownTime, $('#countdown-timer'), startBreak);
+        });
+
+        //Pause and Unpause
+
+        $('body').on('click', '#countdown-timer', function () {
+            if (paused) {
+                clockTimer = $(this).text().split(':');
+                createClock(Number(clockTimer[0] * 60) + Number(clockTimer[1]), $('#countdown-timer'));
+                paused = false;
+            } else {
+                clearInterval(countdown);
+                paused = true;
+            }
+        });
+
+        sessionLength = 25;
+
+        $('#plus-length').click(function () {
+            $('#session-length').html(function (i, sessionLength) {
+                return finalSessionLength = sessionLength * 1 + 1;
+            })
+        })
+
+        $('#minus-length').click(function () {
+            if (finalSessionLength <= 0) {
+                finalSessionLength = 0;
+            } else {
+                $('#session-length').html(function (i, sessionLength) {
+                    return finalSessionLength = sessionLength * 1 - 1;
+                })
+            }
+        })
+
+
+        $("#session-length").append(`${sessionLength}`);
+
+    }
+    startClock()
+
+        breakLength = 5;
+        $('#plus-break').click(function () {
+            $('#session-break').html(function (i, sessionBreak) {
+                return finalBreakLength = sessionBreak * 1 + 1;
+            })
+        })
+
+        $('#minus-break').click(function () {
+            if (finalBreakLength <= 0) {
+                finalBreakLength = 0;
+            } else {
+                $('#session-break').html(function (i, sessionBreak) {
+                    return finalBreakLength = sessionBreak * 1 - 1;
+                })
+            }
+        })
+
+        $("#session-break").append(`${breakLength}`);
     
-    countdown = setInterval(function() {
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.text(minutes + ":" + seconds);
-
-        if (--timer < 0) {
-            clearinterval(countdown);
-        }
-    }, 1000);
-}
-
-$('.start-timer').on('click', function() {
-    if (countdown) {
-        clearInterval(countdown);
-    }
-    paused = false;
-    $('body').append("<div id ='countdown-timer'></div>");
-    countdownTime = 60 * finalSessionLength;
-    startTimer(countdownTime, $('#countdown-timer'));
-});
-//Pause and Unpause
-
-$('body').on('click', '#countdown-timer', function() {
-    if (paused) {
-        var timer = $(this).text().split(':');
-        startTimer(Number(timer[0] * 60) + Number(timer[1]), $('#countdown-timer'));
-        paused = false;
-    } else {
-        clearInterval(countdown);
-        paused = true;
-    }
-    });
-
-    sessionLength = 25;
-
-    $('#plus-length').click(function(){
-        $('#session-length').html(function(i, sessionLength){
-            return finalSessionLength = sessionLength*1+1;
-        })
-    })
-
-    $('#minus-length').click(function(){
-        $('#session-length').html(function(i, sessionLength){
-            return finalSessionLength = sessionLength*1-1;
-        })
-    })
-
-
-    $("#session-length").append(`${sessionLength}`);
-    // console.log("countdownTime", countdownTime);
 });
